66758c269ccbd249ceb5a467141eace53a10b52e
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-05-31 20:43:45 +0200

    Initial API w/o structure - has core dependencies

---
+++ secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/SecureTeam3TodoApiApplication.java
@@ -0,0 +1,13 @@
+package org.team3todo.secure.secure_team_3_todo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureTeam3TodoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureTeam3TodoApiApplication.class, args);
+	}
+
+}
---
+++ secure-team-3-todo-api/src/test/java/org/team3todo/secure/secure_team_3_todo_api/SecureTeam3TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.team3todo.secure.secure_team_3_todo_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureTeam3TodoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
