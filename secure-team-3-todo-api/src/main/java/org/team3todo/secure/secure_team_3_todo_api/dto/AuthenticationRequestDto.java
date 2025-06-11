ce20e5824b4a17bb2381de7209c8f2d00076ad2f
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-06-11 16:43:12 +0200

    Data validation. Needs testing

--- secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/dto/AuthenticationRequestDto.java
+++ secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/dto/AuthenticationRequestDto.java
@@ -1,16 +1,23 @@
 package org.team3todo.secure.secure_team_3_todo_api.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.team3todo.secure.secure_team_3_todo_api.validation.ValidPassword;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthenticationRequestDto {
+    @Size(max = 255, message = "Username cannot exceed 255 characters.")
     private String username;
+    @Email(message = "If provided, email must be in a valid format.")
+    @Size(max = 255, message = "Email cannot exceed 255 characters.")
     private String email;
+    @ValidPassword
     private String password;
 }
--- secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/exception/GlobalExceptionHandler.java
+++ secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/exception/GlobalExceptionHandler.java
@@ -10,13 +10,28 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.team3todo.secure.secure_team_3_todo_api.dto.ErrorResponseDto;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.time.OffsetDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    // This method handles our custom ResourceNotFoundException
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorResponseDto> handleResourceNotFoundException(
             ResourceNotFoundException ex, WebRequest request) {
