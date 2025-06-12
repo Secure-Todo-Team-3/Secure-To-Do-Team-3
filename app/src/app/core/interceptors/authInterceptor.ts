256a01ec3353e114801c25e6bf4357135edeb8c6
brilliant-bbd <162683143+brilliant-bbd@users.noreply.github.com>
2025-06-12 22:08:32 +0200

    Update authInterceptor.ts

--- app/src/app/core/interceptors/authInterceptor.ts
+++ app/src/app/core/interceptors/authInterceptor.ts
@@ -2,6 +2,7 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { environment } from 'src/app/shared/environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(StorageService);
