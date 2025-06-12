70a505544abf4570439eb61e5b78a49232f9e4e2
GregoryMaselle-W <Gregory.Maselle@bbd.co.za>
2025-06-12 13:41:06 +0200

    RL @>@

---
+++ secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/security/SimpleRateLimitingFilter.java
@@ -0,0 +1,46 @@
+package org.team3todo.secure.secure_team_3_todo_api.security;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SimpleRateLimitingFilter implements Filter {
+
+    private final Map<String, Bucket> buckets = new ConcurrentHashMap<>();
+
+    private Bucket createNewBucket() {
+        Bandwidth limit = Bandwidth.classic(60, Refill.greedy(60, Duration.ofMinutes(1)));
+        return Bucket.builder().addLimit(limit).build();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String clientIp = httpRequest.getRemoteAddr();
+        Bucket clientBucket = buckets.computeIfAbsent(clientIp, k -> createNewBucket());
+
+        if (clientBucket.tryConsume(1)) {
+            chain.doFilter(request, response);
+        } else {
+            httpResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            httpResponse.getWriter().write("Too many requests");
+        }
+    }
+}
