97ca6d254aa03fe44722321419e2c372aded96e6
brilliant-bbd <162683143+brilliant-bbd@users.noreply.github.com>
2025-06-12 20:42:07 +0200

    Update SecurityConfig.java

--- secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/security/SecurityConfig.java
+++ secure-team-3-todo-api/src/main/java/org/team3todo/secure/secure_team_3_todo_api/security/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.boot.web.server.Cookie.SameSite;
 
 import java.util.Arrays;
 import java.util.List;
